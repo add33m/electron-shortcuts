8477f939436dfac85c33e7748357a644caba8b56
add33m <48681209+add33m@users.noreply.github.com>
2021-04-01 10:54:09 +0200

    Added CSS for errors/messages and added a message when shortcut array is empty

--- app/src/app/index.js
+++ app/src/app/index.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import Shortcut from './components/shortcut';
 import Overlay from './components/overlay';
@@ -13,6 +14,7 @@ export default class App extends React.Component {
       shortcuts: [],
       loading: true,
       loadError: "",
+      noShortcuts: false, // Display a message if no shortcuts have been added
     };
 
     this.updateConfig = this.updateConfig.bind(this);
@@ -27,12 +29,12 @@ export default class App extends React.Component {
     // Read contents of shortcuts file and save to state
     fs.readFile("./config/shortcuts.json", (err, data) => {
       // Display error if file was not able to be loaded
-      if (err) this.setState({ shortcuts: [], loading: false, loadError: "An error occured loading 'shortcuts.json'. Does it exist?\n" + err });
+      if (err || !data) return this.setState({ shortcuts: [], loading: false, loadError: "An error occured loading 'shortcuts.json'. Does it exist?\n" + err });
 
       // Wrap JSON parse and display error if something happens (parse will fail if JSON is not correctly formatted)
       try {
         const read = JSON.parse(data);
-        this.setState({ shortcuts: read, loading: false });
+        this.setState({ shortcuts: read, loading: false, noShortcuts: !(read.length > 0) });
       } catch (parseErr) {
         this.setState({ shortcuts: [], loading: false, loadError: "An error occured reading 'shortcuts.json'. Is the JSON properly formatted?\n" + parseErr });
       }
@@ -42,10 +44,18 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="main-container">
-        <div className="shortcuts-bg">
-          {/* { this.state.loading && (<p>Loading...</p>) } */}
-          { this.state.loadError && (<p>{this.state.loadError}</p>) }
 
+        {/* { this.state.loading && (<p>Loading...</p>) } */}
+        { this.state.loadError && (<p className="main-error">{this.state.loadError}</p>) }
+        { this.state.noShortcuts && (
+          <p className="main-message">
+            It looks like you haven't added any shortcuts yet!<br />
+            Click the <FontAwesomeIcon icon="pencil-alt" /> at the bottom right to start editing the 'shortcuts.json' file,
+            then click <FontAwesomeIcon icon="sync-alt" /> to load 'em in!
+          </p>
+        ) }
+
+        <div className="shortcuts-bg">
           { this.state.shortcuts.length > 0 && this.state.shortcuts.map(shortcut => <Shortcut {...shortcut} />) }
         </div>
         
